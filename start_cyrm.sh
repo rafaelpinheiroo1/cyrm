9d7a3e3d05b9ee4984da35ce2353218a7fc545b1
rafaelpinheiroo1 <rafaelpinheiro.380@gmail.com>
2022-06-06 22:22:10 +0000

    code update

--- start_cyrm.sh
+++ start_cyrm.sh
@@ -14,33 +14,56 @@
 
 
 
-program_check=$(dpkg -l | grep -i docker)
-if [ -n "$program_check" ]; then
+#docker_check=$(dpkg -l | grep -i docker)
+#if [ -n "$docker_check" ]; then
+#    echo "DOCKER INSTALADO!"
+#else
+#    echo "NÃO TEM DOCKER"
+#    echo "INSTALANDO..."
+#    apt update -y
+#    apt install apt-transport-https ca-certificates curl software-properties-common -y #docker para ubuntu 20.04
+#    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#    apt update -y
+#    apt install docker-ce -y
+#    echo "DOCKER INSTALADO COM SUCESSO!!!"
+#fi
+
+
+
+
+
+docker_check=$(dpkg -l | grep -i docker)
+if [ -n "$docker_check" ]; then
     echo "DOCKER INSTALADO!"
 else
-    echo "NÃO TEM DOCKER"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    echo "DOCKER INSTALADO COM SUCESSO!!!"
+fi
+
+tshark_check=$(dpkg -l | grep -i tshark)
+if [ -n "$tshark_check" ]; then
+    echo "TSHARK INSTALADO!"
+else
+    echo "NÃO TEM TSHARK"
     echo "INSTALANDO..."
-    sudo apt update -y
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y #docker para ubuntu 20.04
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    sudo apt update -y
-    sudo apt install docker-ce -y
-    exho "DOCKER INSTALADO COM SUCESSO!!!"
+    apt install tshark -y
+    echo "TSHARK INSTALADO COM SUCESSO!!!"
 fi
 
 mkdir -p /home/capture
 
-docker build /cyrm/images/blue_team_image/ -t blue-team
-docker build /cyrm/images/ssh_image/ -t ssh-server #mudar o nome do servidor para "servidor web"
-docker build /cyrm/images/user_image/ -t alpine-user
-docker build /cyrm/images/ddos_image/ -t ddos-attack
-docker build /cyrm/images/dic_ssh_image/ -t dic_attack_ssh
+docker build ~/cyrm/images/blue_team_image/ -t blue-team
+docker build ~/cyrm/images/ssh_image/ -t ssh-server #mudar o nome do servidor para "servidor web"
+docker build ~/cyrm/images/user_image/ -t alpine-user
+docker build ~/cyrm/images/ddos_image/ -t ddos-attack
+docker build ~/cyrm/images/dic_ssh_image/ -t dic-attack-ssh
 
 docker pull dperson/samba
 docker pull containernet/containernet
 
-docker run --name containernet -it --r --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock -v /cyrm/topology:/cyrm containernet/containernet python3 /cyrm/scenario-1.py
+docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock -v ~/cyrm/topology:/cyrm containernet/containernet python3 /cyrm/scenario-1.py
 
 
 
